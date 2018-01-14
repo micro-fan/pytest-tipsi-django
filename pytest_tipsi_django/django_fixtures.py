faf3e5edad8c42d39c40b130a6cbafba3f43dbe4
Artsiom Kaval <lezeroq@gmail.com>
2018-01-14 18:50:39 +0300

    Backport django_assert_num_queries from pytest-django

--- pytest_tipsi_django/django_fixtures.py
+++ pytest_tipsi_django/django_fixtures.py
@@ -86,3 +86,27 @@ def module_settings(session_settings):
     wrapper = SettingsWrapper()
     yield wrapper
     wrapper.finalize()
+
+
+try:
+    from pytest_django.fixtures import django_assert_num_queries
+except ImportError:
+    @pytest.fixture(scope='function')
+    def django_assert_num_queries(pytestconfig):
+        from django.db import connection
+        from django.test.utils import CaptureQueriesContext
+
+        @contextmanager
+        def _assert_num_queries(num):
+            with CaptureQueriesContext(connection) as context:
+                yield
+                if num != len(context):
+                    msg = "Expected to perform %s queries but %s were done" % (num, len(context))
+                    if pytestconfig.getoption('verbose') > 0:
+                        sqls = (q['sql'] for q in context.captured_queries)
+                        msg += '\n\nQueries:\n========\n\n%s' % '\n\n'.join(sqls)
+                    else:
+                        msg += " (add -v option to show queries)"
+                    pytest.fail(msg)
+
+        return _assert_num_queries
